aaa49019ea7244abff20711b8d0e3f8170da82c7
ajwlukas <ajwlukas@gmail.com>
2023-01-20 16:02:20 +0900

    Create TL_Luminance.hlsl
    it cacluate Pixels luminance

---
+++ Shader/PS/TL_Luminance.hlsl
@@ -0,0 +1,14 @@
+#include "TL_IO_VS.hlsli"
+
+#include "TL_Samplers.hlsli"
+
+#include "TL_TexturesPS.hlsli"
+
+#include "TL_Reinhard.hlsli"
+
+///픽셀의 휘도를 구하는 쉐이더, GreyScale과 같다고 볼 수 있을 듯.
+
+float4 main(VS_Out_ScreenSpace surface) : SV_TARGET0
+{
+    return float4(Luminance(interPass.Sample(Sampler_Clamp, surface.uv).rgb).r, 0.0f, 0.0f, 1.0f);
+}
