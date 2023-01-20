04e8577ae1a71bcb143e4336e1764b209829eddc
ajwlukas <ajwlukas@gmail.com>
2023-01-20 16:01:53 +0900

    Create TL_DownSampler3x3Average.hlsl
    a Shader DownSampling with average

---
+++ Shader/PS/TL_DownSampler3x3Average.hlsl
@@ -0,0 +1,24 @@
+#include "TL_IO_VS.hlsli"
+
+#include "TL_Samplers.hlsli"
+
+#include "TL_TexturesPS.hlsli"
+
+float4 main(VS_Out_ScreenSpace surface) : SV_TARGET0
+{
+    float4 ret = float4(0, 0, 0, 0);
+    
+    for (int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            // Compute the sum of color values
+            ret += interPass.Sample(Sampler_Clamp, surface.uv, int2(x, y));
+        }
+    }
+    
+    // Divide the sum to complete the average
+    ret /= 9;
+    
+    return float4(ret.rgb, 1.0f);
+}
