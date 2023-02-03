a5d5373e5217dcefc73e86b0b8bfc727405f07cf
ajwlukas <ajwlukas@gmail.com>
2023-02-03 20:32:03 +0900

    Update TestObjectGBuffers.hlsl
    testing IBL using instancing

--- Shader/PS/TestObjectGBuffers.hlsl
+++ Shader/PS/TestObjectGBuffers.hlsl
@@ -10,27 +10,33 @@
 
 #include "TL_PBR.hlsli"
 
-cbuffer roughnessMaterial : register(b10)
+Buffer<float4> MaterialTest : register(t50);
+
+float2 GetMaterialByInstanceID(uint instanceID)
 {
-	float metallic;
-	float roughness;
+    return MaterialTest.Load(instanceID).xy;
 }
-
-
 
 PS_Out main(VS_Out surface)
 {
 	PS_Out ret;
-    
+	
 	float4 basecolor_opacity = baseColor_opcityMap.Sample(Sampler_Wrap, surface.uv);
     
-	ret.out0 = float4(basecolor_opacity.rgb, 1.0f); //albedo
+	//ret.out0 = float4(basecolor_opacity.rgb, 1.0f); //albedo
+    float test = surface.instanceID / 20.0f;
+    ret.out0 = float4(0.98f, 0.82f, 0.76f, 1.0f); //albedo
 
 	ret.out1 = float4(surface.normal_world, 1.0f); //normal_world
     
+    float2 mr = GetMaterialByInstanceID(surface.instanceID);
+	
+    float metallic = mr.x;
+    float roughness = mr.y;
+	
 	ret.out2 = float4(surface.pos_world, 1.0f); //pos_world
-	ret.out3 = float4(metallic, 1.0f, 1.0f, 0.0f); //metalness
-	ret.out4 = float4(max(0.01f, roughness), 0.0f, 0.0f, 0.0f); //roughness
+    ret.out3 = float4(metallic, 1.0f, 1.0f, 0.0f); //metalness
+	ret.out4 = float4(roughness, 0.0f, 0.0f, 0.0f); //roughness
 	ret.out5 = float4(surface.pos_NDC.z, 0.0f, 0.0f, 0.0f);
     
 	return ret;
